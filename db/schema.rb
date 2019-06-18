3db64fff507070ef680a9ccab941954cc7e80eca
Kari O'Neal <triadfirewood@gmail.com>
2019-06-18 14:44:58 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,6 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "costume_stores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
-  end
-
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
@@ -31,15 +21,14 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "haunted_houses", force: :cascade do |t|
+  create_table "costure_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.string "theme"
-    t.float "price"
-    t.boolean "family_friendly"
-    t.datetime "opening_date"
-    t.datetime "closing_date"
-    t.text "description"
+    t.integer "inventory"
+    t.integer "employees"
+    t.boolean "status"
+    t.datetime "open"
+    t.datetime "closed"
   end
 
 end
