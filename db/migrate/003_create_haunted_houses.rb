6c11abe770ccddda1245b7982771dcd425db6fe9
Kari O'Neal <triadfirewood@gmail.com>
2019-06-18 14:42:33 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :location 
       t.string :theme 
       t.float :price 
-      t.boolean :family 
+      t.boolean :family_friendly
     end 
   end 
   
